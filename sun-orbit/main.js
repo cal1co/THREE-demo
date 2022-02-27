d399801bd8a6b5b37a541d198f2903429956fecd
Alex King <calicoalix@gmail.com>
2022-02-27 22:29:45 +1100

    changes to star render

--- sun-orbit/main.js
+++ sun-orbit/main.js
@@ -1,6 +1,7 @@
 import './style.css'
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 
 
 const scene = new THREE.Scene()
@@ -9,7 +10,7 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 
 const renderer = new THREE.WebGLRenderer({
 
-  canvas: document.querySelector('#space'),
+  canvas: document.querySelector('#sun'),
 
 });
 
@@ -17,7 +18,8 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 camera.position.setZ(100); 
-// renderer.render(scene, camera) 
+
+// renderer.render(scene, camera)
 
 const geometry = new THREE.SphereGeometry(15, 32, 16);
 const material = new THREE.MeshToonMaterial({ color:0xF94F00, wireframe:false })
@@ -26,60 +28,50 @@ scene.add(sphere)
 
 // renderer.render(scene, camera)
 
+
 const pointLight = new THREE.PointLight(0xffffff)
 pointLight.position.set(100, 100, 100)
 const ambientLight = new THREE.AmbientLight(0xffffff)
 scene.add(ambientLight)
 
-// const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(400, 50)
+
+const gridHelper = new THREE.GridHelper(200,50) // shows us a level plane for perspective 
 // scene.add(gridHelper)
 
-const controls = new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement);
+
 
 function addStar(){
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({color: 0xffffff})
-  const star = new THREE.Mesh(geometry, material)
+  const geometry = new THREE.SphereGeometry(0.25,24,24); // create new sphere
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff}) // give this sphere a mesh
+  const star = new THREE.Mesh(geometry, material) // initialise the stars 
 
-  const [x, y, z] = Array(3).fill().map( () => THREE.MathUtils.randFloatSpread( 500 ))
+  const [x, y, z] = Array(3).fill().map( () => THREE.MathUtils.randFloatSpread( 500 )) // create an array with length 3, then add a random float with max of 100 
 
-  star.position.set(x, y, z)
-
-  scene.add(star)
+  star.position.set(x, y, z); //set the x, y, z
+  scene.add(star) // add to the scene 
 }
 
-Array(100).fill().forEach(addStar)
+Array(400).fill().forEach(addStar) // create an array of 200 and for each, call addStar
 
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
   new THREE.MeshStandardMaterial({color: 0xA09BD8})
 )
+// moon.position.setZ(30)
 scene.add(moon)
 
-// moon.position.setZ(30)
 const orbitRadius = 30
-let goForward = false 
-let moonVar = 50
+let moonVar = 1
 
-function animate(){
-  requestAnimationFrame(animate)
 
-  // sphere.rotation.x += 0.01
-  // sphere.rotation.y += 0.01
+function animate(){ // recursive loop that renders the animation: game loop
+  requestAnimationFrame(animate);
 
-  if (moonVar <= 50 && goForward == false){
+  if (moonVar <= 1){
     moonVar -= 0.01
-    if (moonVar === 0){
-      goForward = true
-    }
   } 
-  if (moonVar < 50 && goForward == true){
-    moonVar += 0.01
-    if (moonVar === 50){
-      goForward = false
-    }
-  } 
+  
   moon.position.set(
     Math.cos(moonVar) * orbitRadius,
     Math.sin(moonVar) * orbitRadius
@@ -87,6 +79,7 @@ function animate(){
 
   controls.update()
 
-  renderer.render(scene, camera)
+  renderer.render(scene, camera);
 }
 animate()
+
